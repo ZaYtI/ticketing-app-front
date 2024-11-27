81f0a438438f63b903ea9c5ba130db0bc988ee3e
ZaYtI <antonin_riquart@ens.univ-artois.fr>
2024-11-27 19:41:29 +0100

    🐛 Fix credential bug

--- composables/auth.ts
+++ composables/auth.ts
@@ -1,14 +1,15 @@
-export async  function login(email: string, password: string) {
-  const response = await $fetch(`http://localhost:8000/api/login?`,
-    {
-      method: "POST",
-      mode: "cors",
-      body:{
-        'email':email,
-        'password':password
-      }
-    }
-  );
+export async function login(email: string, password: string) {
+  const response = await $fetch(`http://localhost:8000/api/login`, {
+    method: "POST",  
+    headers: {
+      "Content-Type": "application/json",  
+    },
+    credentials: "include",  
+    body: {
+      'email': email,
+      'password': password,
+    },  
+  });
 
-  return response;
+  return response;  
 }
